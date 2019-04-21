0be90118b86a57734c9050f2612071ab6656c7be
NanoSector <rick@nanosector.nl>
2019-04-21 19:21:32 +0200

    Declare strict types check
    Signed-off-by: NanoSector <rick@nanosector.nl>

--- tests/IrcMessageTest.php
+++ tests/IrcMessageTest.php
@@ -5,6 +5,7 @@
  * You should have received a copy of the MIT license with the project.
  * See the LICENSE file for more information.
  */
+declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 use WildPHP\Messages\Raw;
--- tests/MessageCasterTest.php
+++ tests/MessageCasterTest.php
@@ -1,9 +1,11 @@
-<?php /**
+<?php
+/**
  * Copyright 2019 The WildPHP Team
  *
  * You should have received a copy of the MIT license with the project.
  * See the LICENSE file for more information.
  */
+declare(strict_types=1);
 
 /** @noinspection PhpUnhandledExceptionInspection */
 
